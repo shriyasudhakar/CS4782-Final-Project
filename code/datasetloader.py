f37a206cb7e520d9107e2e0aef0912a52189e740
Shriya Sudhakar <shriyasudhakar@dhcp-vl2041-19393.eduroam.cornell.edu>
2026-04-12 16:22:15 -0400

    create dataloader and ipynb for training

---
+++ code/datasetloader.py
@@ -0,0 +1,24 @@
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+import numpy as numpy
+import os
+
+# Section 3.3: "we apply standard data augmentation procedures including horizontal flipping and random cropping to cover image C in our pre-processing pipeline"
+
+class Div2KDataset(Dataset):
+    def __init__(self, root, transforms=None):
+        self.path_to_img = []
+        for f in os.listdir(root):
+            if f.endswith('png'):
+                self.path_to_img.append(os.path.join(root, f))
+
+        self.transforms = transforms
+
+    def __len__(self):
+        return len(self.path_to_img)
+    
+    def __getitem__(self, idx):
+        image = Image.open(self.path_to_image[idx]).convert('RGB')
+        if self.transform:
+            image = self.transform(image)
+        return image
